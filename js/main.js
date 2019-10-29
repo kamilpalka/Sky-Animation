e111277156698b3215611bb69ad2a133189a0657
Kamil Pałka <palkakamil1988@gmail.com>
2019-10-29 22:21:51 +0100

    add stars constelations

--- js/main.js
+++ js/main.js
@@ -60,6 +60,51 @@ class Sky {
     });
   }
 
+  generateRandomConstellation() {
+    const x = this.width / 2 + Math.random() * this.width - this.width / 2;
+    const y = this.height / 2 + Math.random() * this.height - this.height / 2;
+    const radius = (this.height / 2) * Math.random() * 0.5 + 0.5;
+
+    this.constellation = {
+      stars: this.stars
+        .filter(star => {
+          return (
+            star.x > x - radius &&
+            star.x < x + radius &&
+            star.y > y - radius &&
+            star.y < y + radius
+          );
+        })
+        .slice(0, Math.round(Math.random() * 7 + 3)),
+      isClosed: Math.random() > 0.5
+    };
+  }
+
+  drawConstellation() {
+    const { stars, isClosed } = this.constellation;
+    const starsCount = stars.length;
+
+    if (starsCount > 2) {
+      const firstStar = stars[0];
+
+      this.ctx.beginPath();
+      this.ctx.moveTo(firstStar.x, firstStar.y);
+      this.ctx.lineTo(stars[1].x, stars[1].y);
+
+      for (let i = 1; i < starsCount - 1; i++) {
+        const nextStar = stars[i + 1];
+        this.ctx.lineTo(nextStar.x, nextStar.y);
+      }
+
+      if (isClosed) {
+        this.ctx.lineTo(firstStar.x, firstStar.y);
+      }
+
+      this.ctx.strokeStyle = "#f7eada";
+      this.ctx.stroke();
+    }
+  }
+
   clearCanvas() {
     this.ctx.fillStyle = "#000";
     this.ctx.fillRect(0, 0, this.width, this.height);
@@ -91,6 +136,7 @@ class Sky {
     this.clearCanvas();
     this.drawStars();
     this.updateStars();
+    this.drawConstellation();
     this.drawOverlayer();
     window.requestAnimationFrame(() => this.draw());
   }
@@ -98,6 +144,7 @@ class Sky {
   run() {
     this.initCanvas();
     this.gnerateStars(400);
+    this.generateRandomConstellation();
     this.draw();
   }
 }
